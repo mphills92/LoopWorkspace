693a4b8223a887a38de1aeaff97cc103418bdcfe
mphills92 <mp_hills@outlook.com>
2016-07-25 14:39:03 -0500

    Login view controller major changes implemented.

--- carpoolApp_v1.0/LoginViewController.swift
+++ carpoolApp_v1.0/LoginViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var mainTitle: UILabel!
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+    @IBOutlet weak var loginBackgroundViewCenterYConstraint: NSLayoutConstraint!
+
     @IBOutlet weak var signUpButton: UIButton!
     
     @IBOutlet weak var cancelButton: UIButton!
@@ -27,10 +30,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     var screenSize = UIScreen.mainScreen().bounds
     
     var userWantsToCancel = Bool()
+    var userHasStartedLoginProcess = Bool()
     var emailToValidate = String()
     var emailFieldHasEditedPrior = Bool()
     var passwordToValidate = String()
-
+    
+    var logoImageViewTransformHeight = CGFloat()
     
     let userEmail = UserEmail()
     let userPassword = UserPassword()
@@ -44,12 +49,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         backgroundView.layer.cornerRadius = 8
         
-        signUpButton.layer.cornerRadius = 20
+        signUpButton.layer.cornerRadius = 15
         signUpButton.layer.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.4).CGColor
         signUpButton.layer.borderColor = UIColor.whiteColor().CGColor
         signUpButton.layer.borderWidth = 1
         
-        cancelButton.layer.cornerRadius = 20
+        cancelButton.layer.cornerRadius = 15
         cancelButton.layer.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.4).CGColor
         cancelButton.layer.borderColor = UIColor.whiteColor().CGColor
         cancelButton.layer.borderWidth = 1
@@ -71,34 +76,42 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         emailTextField.attributedPlaceholder = NSAttributedString(string:"Email", attributes: [NSForegroundColorAttributeName: UIColor.blackColor()])
         passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password", attributes: [NSForegroundColorAttributeName: UIColor.blackColor()])
         
-        self.loginButtonCenterConstraint.constant = screenSize.width
-        self.cancelButtonCenterConstraint.constant = screenSize.width
+        //self.loginButtonCenterConstraint.constant = screenSize.width
+        //self.cancelButtonCenterConstraint.constant = screenSize.width
+        self.loginButton.alpha = 0
+        self.cancelButton.alpha = 0
+        
+        userHasStartedLoginProcess = false
         
         emailFieldHasEditedPrior = false
         
         emailTextField.tag = 1
         passwordTextField.tag = 2
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
+    
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: self.view.window)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: self.view.window)
+        
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
 }
 
 extension LoginViewController {
     
     func textFieldDidBeginEditing(textField: UITextField) {
-        switch (textField.tag) {
-        case 1:
-            if (emailFieldHasEditedPrior == false) {
-                cancelButtonEntersFromRight()
-                loginButtonOutOfView()
-                emailFieldHasEditedPrior = true
-            } else {
-                cancelButtonEntersFromRight()
-                loginButtonOutOfView()
-            }
-        case 2:
-            emailTextField.resignFirstResponder()
-        default:
-            break
-        }
+        cancelButtonEnters()
+        loginButtonEnters()
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
@@ -106,13 +119,16 @@ extension LoginViewController {
         case 1:
             if (userWantsToCancel == true) {
                 emailTextField.resignFirstResponder()
-                cancelButtonExitsToRight()
+                userHasStartedLoginProcess = false
+                cancelButtonExits()
+                loginButtonExits()
+                
             } else {
                 emailToValidate = emailTextField.text!
                 passwordTextField.becomeFirstResponder()
+                userHasStartedLoginProcess = true
                 emailFieldHasEditedPrior = true
                 userWantsToCancel = false
-                loginButtonInView()
             }
         case 2:
             passwordToValidate = passwordTextField.text!
@@ -137,16 +153,23 @@ extension LoginViewController {
     
     // Evaluate credentials upon pressing Login button.
     @IBAction func loginButtonPressed(sender: AnyObject) {
-    
         validatedEmail()
         validatedPassword()
         
-        passwordTextField.resignFirstResponder()
-        
         if (emailTextField.text! == "" || passwordTextField.text! == "") {
+            
+            
             let alertController = UIAlertController(title: "You forgot to enter either an email or password.", message:  "\n Please enter your full email and password before attempting to login.", preferredStyle: .Alert)
+            
+            
+                self.emailTextField.resignFirstResponder()
+                self.passwordTextField.resignFirstResponder()
+                self.clearTextFields()
+                self.cancelButtonExits()
+                self.loginButtonExits()
+            
             let doneAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
-                self.passwordTextField.becomeFirstResponder()
+                self.userHasStartedLoginProcess = false
             }
             alertController.addAction(doneAction)
             
@@ -161,10 +184,18 @@ extension LoginViewController {
                 self.performSegueWithIdentifier("toForgotPasswordSegue", sender: self)
             }
             alertController.addAction(forgotPasswordAction)
+            
+                self.emailTextField.resignFirstResponder()
+                self.passwordTextField.resignFirstResponder()
+                self.clearTextFields()
+                self.cancelButtonExits()
+                self.loginButtonExits()
+            
             let doneAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
-                self.passwordTextField.becomeFirstResponder()
+                self.userHasStartedLoginProcess = false
             }
             alertController.addAction(doneAction)
+            
             self.presentViewController(alertController, animated: true) {
                 alertController.view.tintColor = UIColor(red: 0/255, green: 51/255, blue: 0/255, alpha: 1.0)
             }
@@ -177,53 +208,84 @@ extension LoginViewController {
     @IBAction func cancelButtonPressed(sender: AnyObject) {
         userWantsToCancel = true
         emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        userHasStartedLoginProcess = false
+        clearTextFields()
+        cancelButtonExits()
+        loginButtonExits()
+        
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(sender: AnyObject) {
+        self.performSegueWithIdentifier("toForgotPasswordSegue", sender: self)
+    }
+    
+    
+    func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardHeight = (notification.userInfo! as NSDictionary).objectForKey(UIKeyboardFrameBeginUserInfoKey)?.CGRectValue.size.height else {
+            return
+        }
+        
+        if (userHasStartedLoginProcess == false) {
+            loginBackgroundViewCenterYConstraint.constant -= ((screenSize.height - keyboardHeight) / 4)
+            logoImageViewTransformHeight = ((((screenSize.height - keyboardHeight) / 4) - 50) / 100) * 1.25
+        UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
+            self.logoImageView.transform = CGAffineTransformMakeScale(self.logoImageViewTransformHeight, self.logoImageViewTransformHeight)
+            self.mainTitle.alpha = 0
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        
+            view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        loginBackgroundViewCenterYConstraint.constant = 0
+        UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
+            self.logoImageView.transform = CGAffineTransformMakeScale(1, 1)
+            self.mainTitle.alpha = 1.0
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        view.layoutIfNeeded()
     }
     
     
     // Cancel button slides into view from right to left.
-    func cancelButtonEntersFromRight() {
+    func cancelButtonEnters() {
         userWantsToCancel = false
-        self.cancelButtonCenterConstraint.constant = self.screenSize.width
+        //self.cancelButtonCenterConstraint.constant = self.screenSize.width
         UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 5, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-            self.cancelButtonCenterConstraint.constant -= self.screenSize.width
+            self.cancelButton.alpha = 1.0
+            //self.cancelButtonCenterConstraint.constant -= self.screenSize.width
             self.view.layoutIfNeeded()
             }, completion: nil)
     }
     
     // Cancel button slides out of view from left to right.
-    func cancelButtonExitsToRight() {
+    func cancelButtonExits() {
         userWantsToCancel = false
-        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.cancelButtonCenterConstraint.constant = self.screenSize.width
+        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            self.cancelButton.alpha = 0
+            //self.cancelButtonCenterConstraint.constant = self.screenSize.width
             self.view.layoutIfNeeded()
             }, completion: nil)
     }
 
     // Login button slides into view from right to left.
-    func loginButtonInView() {
-        
-        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
-            self.cancelButton.alpha = 0
-            self.view.layoutIfNeeded()
-            }, completion: nil)
-        
-        self.loginButtonCenterConstraint.constant = self.screenSize.width
+    func loginButtonEnters() {
+        //self.loginButtonCenterConstraint.constant = self.screenSize.width
         UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 5, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.loginButtonCenterConstraint.constant -= self.screenSize.width
+            self.loginButton.alpha = 1.0
+            //self.loginButtonCenterConstraint.constant -= self.screenSize.width
             self.view.layoutIfNeeded()
             }, completion: nil)
     }
     
     // Login button slides out of view from left to right.
-    func loginButtonOutOfView() {
-        
-        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
-            self.cancelButton.alpha = 1.0
-            self.view.layoutIfNeeded()
-            }, completion: nil)
-        
-        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-            self.loginButtonCenterConstraint.constant = self.screenSize.width
+    func loginButtonExits() {
+        UIView.animateWithDuration(0.1, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.loginButton.alpha = 0
+            //self.loginButtonCenterConstraint.constant = self.screenSize.width
             self.view.layoutIfNeeded()
             }, completion: nil)
     }
@@ -242,5 +304,10 @@ extension LoginViewController {
         } else {
             return false
         }
+    }
+    
+    func clearTextFields() {
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
 }
