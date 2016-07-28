04871778ac07b5bc5b46a838453698d9f947b474
mphills92 <mp_hills@outlook.com>
2016-07-28 00:08:11 -0500

    Keyboard notifications altered for Late Caddie view controller to adjust based on keyboard auto correct swipe bar.

--- carpoolApp_v1.0/LateCaddieViewController.swift
+++ carpoolApp_v1.0/LateCaddieViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class LateCaddieViewController: UIViewController, UITextFieldDelegate {
+class LateCaddieViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var closeTextFieldButton: UIButton!
-    @IBOutlet weak var extraInfoTextField: UITextField!
+    @IBOutlet weak var infoTextView: UITextView!
     
     @IBOutlet weak var thirdParagraphToTopReferenceConstraint: NSLayoutConstraint!
     @IBOutlet weak var topConstraintInfoTextFieldBackground: NSLayoutConstraint!
@@ -23,7 +23,7 @@ class LateCaddieViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        extraInfoTextField.delegate = self
+        infoTextView.delegate = self
         
         navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
         navigationController?.navigationBar.barStyle = UIBarStyle.Default
@@ -31,19 +31,22 @@ class LateCaddieViewController: UIViewController, UITextFieldDelegate {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
         navigationController?.navigationBar.shadowImage = UIImage()
         
-        extraInfoTextField.layer.cornerRadius = 8
+        infoTextView.layer.cornerRadius = 8
+        infoTextView.text = "Please enter any additional details here."
+        infoTextView.textColor = UIColor.lightGrayColor()
         closeTextFieldButton.hidden = true
         
         submitButton.layer.cornerRadius = 20
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(self.keyboardNotification(_:)),
+                                                         name: UIKeyboardWillChangeFrameNotification,
+                                                         object: nil)
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: self.view.window)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: self.view.window)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 }
 
@@ -54,7 +57,7 @@ extension LateCaddieViewController {
     }
     
     @IBAction func closeTextFieldButtonPressed(sender: AnyObject) {
-        extraInfoTextField.resignFirstResponder()
+        infoTextView.resignFirstResponder()
     }
     
     
@@ -63,23 +66,52 @@ extension LateCaddieViewController {
         self.dismissViewControllerAnimated(true, completion: {})
     }
     
-    func keyboardWillShow(notification: NSNotification) {
-        guard let keyboardHeight = (notification.userInfo! as NSDictionary).objectForKey(UIKeyboardFrameBeginUserInfoKey)?.CGRectValue.size.height else {
-            return
+    func textViewDidBeginEditing(textView: UITextView) {
+        if (textView.textColor == UIColor.lightGrayColor()) {
+            textView.text = nil
+            textView.textColor = UIColor.darkGrayColor()
         }
-        closeTextFieldButton.hidden = false
-        topConstraintInfoTextFieldBackground.constant -= 270
-        bottomConstraintTextFieldDistanceMoved = ((screenSize.height - keyboardHeight) - (screenSize.height - 80))
-        bottomConstraintInfoTextFieldBackground.constant -= bottomConstraintTextFieldDistanceMoved
-        
-        view.layoutIfNeeded()
     }
     
-    func keyboardWillHide(notification: NSNotification) {
-        closeTextFieldButton.hidden = true
-        topConstraintInfoTextFieldBackground.constant += 270
-        bottomConstraintInfoTextFieldBackground.constant += bottomConstraintTextFieldDistanceMoved
-        view.layoutIfNeeded()
+    func textViewDidEndEditing(textView: UITextView) {
+        if (textView.text.isEmpty) {
+            textView.text = "Please enter any additional details here."
+            textView.textColor = UIColor.lightGrayColor()
+        }
     }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue()
+            let duration:NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.unsignedLongValue ?? UIViewAnimationOptions.CurveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrame?.origin.y >= UIScreen.mainScreen().bounds.size.height {
+                self.topConstraintInfoTextFieldBackground.constant = 280
+                self.bottomConstraintTextFieldDistanceMoved = 80
+                self.bottomConstraintInfoTextFieldBackground.constant = bottomConstraintTextFieldDistanceMoved
+                closeTextFieldButton.hidden = true
+            } else {
+                self.topConstraintInfoTextFieldBackground.constant = 0
+                self.bottomConstraintInfoTextFieldBackground.constant = (endFrame?.size.height)!
+                closeTextFieldButton.hidden = false
+            }
+            UIView.animateWithDuration(duration,
+                                       delay: NSTimeInterval(0),
+                                       options: animationCurve,
+                                       animations: { self.view.layoutIfNeeded() },
+                                       completion: nil)
+        }
+    }
+    
     
 }
